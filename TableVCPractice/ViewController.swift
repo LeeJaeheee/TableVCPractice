fafe3da37cccbbd54763bf8a2bed95deeff597e8
LeeJaeheee <sns3434@gmail.com>
2024-01-06 12:43:59 +0900

    Initial Commit

---
+++ TableVCPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableVCPractice
+//
+//  Created by 이재희 on 1/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
